f34734fc86609eff1d27c9d86478c097ba17b5a7
justme-trimps <62184360+justme-trimps@users.noreply.github.com>
2020-07-29 21:16:17 +0200

    Update trimps-u2-helper-scripts-u2-melt-6.js

--- scripts/trimps-u2-helper-scripts-u2-melt-6.js
+++ scripts/trimps-u2-helper-scripts-u2-melt-6.js
@@ -1221,6 +1221,119 @@ var cookieOptimizerInterval = setInterval(function() {
 	}
 }, 300 - Math.floor(Math.random() * 100));
 
+
+var tryOptimizeVoids = true;
+var voidsOptimizerCookieName = 'trimps-melt-voids-save';
+
+function setVoidSaveCookie() {
+	setCookie(voidsOptimizerCookieName, game.global.totalRadPortals + '###' + game.buildings.Tribute.owned + '###'+ game.global.totalVoidMaps + '###' + save(true), 10);
+}
+
+var shouldLoadVoidsOptimizedSave = function(reset, tributes, voidMaps, debug) {
+	if (debug) {
+		//console.log('shouldLoadVoidsOptimizedSave');
+	}
+	
+	if (reset != game.global.totalRadPortals) {
+		if (debug) {
+			//console.log('shouldLoadVoidsOptimizedSave wrong portal ' + reset + " " + game.global.totalRadPortals);
+		}
+		return false;
+	}
+
+	if (voidMaps > game.global.totalVoidMaps) {
+		console.log('shouldLoadVoidsOptimizedSave save has more void maps ' + voidMaps + " " + game.global.totalVoidMaps);
+		return true;
+	}
+	
+	return false;
+}
+
+var shouldSaveVoidsOptimizedSave = function(reset, tributes, voidMaps, debug) {
+	if (debug) {
+		//console.log('shouldSaveVoidsOptimizedSave');
+	}
+	
+	if (game.global.totalRadPortals < reset) {
+		if (debug) {
+			//console.log('shouldSaveVoidsOptimizedSave false: previous portal ' + reset + " " + game.global.totalRadPortals);
+		}
+		return false;
+	}
+	
+	if (game.global.totalRadPortals > reset) {
+		if (debug) {
+			console.log('shouldSaveVoidsOptimizedSave true: next portal ' + reset + " " + game.global.totalRadPortals);
+		}
+		return true;
+	}
+
+	if (voidMaps < game.global.totalVoidMaps) {
+		if (debug) {
+			console.log('shouldSaveVoidsOptimizedSave true: more void maps than save ' + voidMaps + " " + game.global.totalVoidMaps);
+		}
+		return true;
+	}
+	
+	//if (voidMaps == game.global.totalVoidMaps && tributes < game.buildings.Tribute.owned) {
+	//	if (debug) {
+	//		console.log('shouldSaveVoidsOptimizedSave true: more tributes than save ' + tributes + " " + game.buildings.Tribute.owned);
+	//	}
+	//	return true;
+	//}
+	
+	if (debug) {
+		//console.log('shouldSaveVoidsOptimizedSave do nothing ' 
+		//	+ reset + " " + game.global.totalRadPortals
+		//	+ " " + voidMaps + " " + game.global.totalVoidMaps
+		//	+ " " + tributes + " " + game.buildings.Tribute.owned
+		//);
+	}
+	
+	return false;
+}
+
+if (cookieVoidsOptimizerInterval) { clearInterval(cookieVoidsOptimizerInterval); cookieVoidsOptimizerInterval = null; }
+var cookieVoidsOptimizerInterval = setInterval(function() { 
+	if (!tryOptimizeVoids)
+		return;
+	
+	if (game.global.world >= voidMapZone
+		|| game.global.world <= (voidMapZone - 5))
+	return;
+
+	//if (debugOptimizer)
+		//console.log('cookieVoidsOptimizerInterval');
+	
+	var cookieValue = getCookie(voidsOptimizerCookieName);
+	
+	if (cookieValue) {
+		if (debugOptimizer)
+			console.log(cookieValue.substring(0, 100));
+		
+		var reset = cookieValue.split('###')[0];
+		var tributes = cookieValue.split('###')[1];
+		var voidMaps = cookieValue.split('###')[2];
+		var saveString = cookieValue.split('###')[3];
+		
+		if (shouldLoadVoidsOptimizedSave(reset, tributes, voidMaps, debugOptimizer)) {
+			if (debugOptimizer)
+				console.log('loading save');
+			
+			load(saveString);
+		} else if (shouldSaveVoidsOptimizedSave(reset, tributes, voidMaps, debugOptimizer)) {
+				setVoidSaveCookie();
+		}
+		
+	} else {
+		if (debugOptimizer)
+			console.log('empty cookie');
+
+		setVoidSaveCookie();
+	}
+}, 500 - Math.floor(Math.random() * 100));
+
+
 //TMP REMOVE
 var lastEggMessage = '';
 setInterval(function() {
