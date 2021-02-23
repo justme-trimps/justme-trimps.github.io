97ce9042d9d02187c1943485f4406173b5149f4f
justme-trimps <62184360+justme-trimps@users.noreply.github.com>
2021-02-23 13:24:25 +0100

    c

--- scripts/trappa.js
+++ scripts/trappa.js
@@ -29,7 +29,7 @@ var refixBoard = function() {
 
 fixBoard();
 var autoExportSave = true;
-var autoExportSaveFrequencyInMinutes = 2;
+var autoExportSaveFrequencyInMinutes = 0.25;
 
 if (autoSaveInterval) { clearInterval(autoSaveInterval); autoSaveInterval = null; }
 var autoSaveInterval = setInterval(function() {
@@ -41,44 +41,35 @@ var autoSaveInterval = setInterval(function() {
 	}
 }, 1000 * 60 * autoExportSaveFrequencyInMinutes);
 
-var lastMapZone = 0;
-var shouldFightSomeMap = function() {
-	if (game.global.mapsActive)
-		return false;
-	
-	if (game.global.mapBonus > 0)
-		return false;
-	
-	if (game.global.world > 10 && game.global.world % 10 == 5 && game.global.world > lastMapZone)
-		return true;
-	
-	return false;
-}
+var stopBuyingUpgrades = false;
 
-if (repeatMaps) { clearInterval(repeatMaps); repeatMaps = null; }
-var repeatMaps = setInterval(function() {
-	if (shouldFightSomeMap()) {
-		if (!game.global.switchToMaps) {
-			mapsClicked();
-		}
-		lastMapZone = game.global.world;
+
+if (buyUpgradesInterval) { clearInterval(buyUpgradesInterval); buyUpgradesInterval = null; }
+var buyUpgradesInterval = setInterval(function() {
+	if (stopBuyingUpgrades)
+		return;
+		
+	var upgrades = document.getElementById('upgradesHere').getElementsByClassName('thingColorCanAfford');
+	for (var i = upgrades.length - 1; i >= 0; i--) {
+		var button = upgrades[i];
+		if (button.getAttribute('id') !== 'Coordination')
+			button.click();
 	}
-}, 1200);
+}, 3000);
 
+var stopBuying = false;
+if (buyBuildingsInterval) { clearInterval(buyBuildingsInterval); buyBuildingsInterval = null; }
+var buyBuildingsInterval = setInterval(function() {
+	if (stopBuying) return;
+	
+	buyBuilding("Hut");
+	buyBuilding("House");
+	buyBuilding("Mansion");
+	buyBuilding("Hotel");
+	buyBuilding("Resort");
+	buyBuilding("Collector");
+	buyBuilding("Tribute");
+	
+}, 50);
 
-//TMP REMOVE
-var lastEggMessage = ''
-setInterval(function() {
-	var eggCells = document.getElementsByClassName("eggCell");
-	if (eggCells.length && eggCells[0].offsetParent != null) {
-		eggCells[0].click();
-		setTimeout(function() {
-			if (document.getElementsByClassName("eggMessage").length && document.getElementsByClassName("eggMessage")[document.getElementsByClassName("eggMessage").length - 1].innerText != lastEggMessage) {
-				lastEggMessage = document.getElementsByClassName("eggMessage")[document.getElementsByClassName("eggMessage").length - 1].innerText;
-				if (lastEggMessage.indexOf("food!") === -1 && lastEggMessage.indexOf("wood!") === -1 && lastEggMessage.indexOf("metal!") === -1) {
-					console.log(getPortalTime() + " " +lastEggMessage);
-				}
-			}
-		}, 200);
-	}
-}, 1000)
+if (switchHeirloomInterval) { clearInterval(switchHeirloomInterval); switchHeirloomInterval = null; }
